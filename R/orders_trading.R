75c9d96241f66fbe3b8fdb756c54879afa3ebfd2
Michael Cawtom <michaelcawtom@Michaels-MBP.attlocal.net>
2019-06-13 13:20:14 -0500

    Added td_get_account() function; generalized td_order() by making account_no a anrgument to the fn

--- R/orders_trading.R
+++ R/orders_trading.R
@@ -7,17 +7,18 @@
 #' @param quantity Number of shares OR contracts in case of options
 #' @param price Applies to LIMIT orders only; the price at which to execute
 #' @param instruction "BUY" or "SELL" or *MANY* others
+#' @param account_no TD account #
 #'
 #' @return The reply to the POST command includes an order # in the header
 #' @export
 #'
 #' @examples
-#' td_order(symbol = "TSLA", "LIMIT", 10, price = 300, )
-td_order <- function(symbol, orderType = "LIMIT", quantity = 0, price, instruction = "BUY") {
+#' td_order(symbol = "TSLA", "LIMIT", 10, price = 300, "SELL")
+td_order <- function(symbol, orderType = "LIMIT", quantity = 0, price, instruction = "BUY", account_no = "489837238") {
 
     #apiverb needs to contemplate "GET, PUT, POST, DELETE, OPTIONS, HEAD, PATCH"
 
-    url <- 'https://api.tdameritrade.com/v1/accounts/489837238/orders' ## need to generalize this
+    url <- paste0("https://api.tdameritrade.com/v1/accounts/",account_no, "/orders") ## need to generalize this
 
     json_to_send <- jsonlite::toJSON(
         tibble(session = "NORMAL",
@@ -81,4 +82,19 @@ td_delete_order <- function(order_number) {
     httr::DELETE(order_url, httr::add_headers(Authorization  =
                                                   paste0("Bearer ", refresh_tokens$access_token))
     )
+}
+
+td_get_account <- function(account_no = "489837238", fields = c("positions", "orders")) {
+
+    url <- paste0("https://api.tdameritrade.com/v1/accounts/",
+                  account_no,
+                  "fields=",
+                  paste(fields, collapse = ","))
+
+    #json_to_send
+    r <- httr::GET(url = url, httr::add_headers( .headers =
+                                                c("Authorization" = paste0("Bearer ", refresh_tokens$access_token),
+                                                                             "Content-Type" = "application/json"))
+    )
+    r
 }
